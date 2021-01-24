ef36af0848498eb6f371124f0aec13b86aa3ff00
jesusmata10 <jesusmata10@gmail.com>
2021-01-24 12:33:07 -0400

    modulo edit

--- router.js
+++ router.js
@@ -19,6 +19,18 @@ router.get('/create', (req, res)=> {
 	res.render('create');
 })
 
+router.get('/edit/:id', (req, res)=> {
+
+	const id = req.params.id;
+	conexion.query('SELECT * FROM users WHERE id= ?',[id], (error, results) => {
+		if (error){
+  			throw error;
+	  	}else{
+	  		res.render('edit',{user:results[0]});
+	  	}
+	})
+})
+
 const crud = require('./controllers/crud');
 router.post('/save',crud.save)
 
